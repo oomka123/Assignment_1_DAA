02a7d98cf8fa545b0858562a8e550d86554b6a1c
oomka123 <k.ahmetov123@gmail.com>
2025-09-28 02:35:01 +0500

    feat(cli): parse args, run algos, emit CSV

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,12 @@
 package org.example;
 
-import org.example.metrics.Metrics;
-import org.example.sortings.MergeSort;
-
-import java.util.Arrays;
+import org.example.cli.CLI;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("...");
 
-        Metrics metrics = new Metrics();
+        CLI cli = new CLI(args);
+        cli.run();
 
-        int[] arr = {3,4,5,2,3,9,7,8};
-        MergeSort.mergeSort(arr, metrics);
-        System.out.println(Arrays.toString(arr));
     }
 }
